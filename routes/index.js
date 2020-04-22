7f4c17c209f44d46f98118f1b750a093d3c75477
Rajat Lohia <rajatlohia1108@gmail.com>
2020-04-23 01:21:24 +0530

    added users and profile route

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const homeController= require('../contollers/home_controller');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
 
 module.exports = router;
