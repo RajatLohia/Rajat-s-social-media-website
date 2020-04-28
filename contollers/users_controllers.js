9b4d6c0f0a784b0014b3b2f47cd253e1b914c272
Rajat Lohia <rajatlohia1108@gmail.com>
2020-04-28 20:35:40 +0530

    signup stage completed, now switching branch

--- contollers/users_controllers.js
+++ contollers/users_controllers.js
@@ -1,5 +1,45 @@
+const User=require('../models/users');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile',{
         rajat:"HOME"
     });
+}
+
+//rendered the sign up page
+module.exports.signUp= function(req,res){
+    return res.render('user_sign_up',{
+        rajat:"codeial |sign up"
+    })
+}
+
+//rendered the sign in page
+module.exports.signIn= function(req,res){
+    return res.render('user_sign_in',{
+        rajat:"codeial |sign in"
+    })
+}
+
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+   User.findOne({email:req.body.email},function(err,user){
+       if(err){console.log('error in finding the user in signing up'); return}
+       
+       if(!user){
+           User.create(req.body,function(err,user){
+            
+            if(err){console.log('error in finding the user WHILE signing up'); return}
+            
+            return res.redirect('/users/sign-in'); 
+           })
+       }else{
+           return res.redirect('back');
+       }
+   });
+}
+
+module.exports.createSession= function(req,res){
+    //todolater
 }
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const usersController= require('../contollers/users_controllers');
 
 router.get('/profile',usersController.profile);
 
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+router.post('/create',usersController.create);
+
 module.exports= router;
